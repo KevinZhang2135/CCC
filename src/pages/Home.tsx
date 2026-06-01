5a5b2fd7ee66530505a600cabcac5600f34f35cc
Kevin Zhang <kevinzhang@Mac.wlan>
2026-05-31 22:02:01 -0400

    Github pages path problem

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-
 import "../index.css"
 
-// import HeroNavBar from "../components/HomePage/HeroNavbar";
 import Hero from "../components/HomePage/Hero";
 import Events from "../components/HomePage/Events";
 import WebApply from "../components/HomePage/WebApply";
@@ -10,12 +7,13 @@ import Join from "../components/HomePage/Join";
 import FAQ from "../components/HomePage/FAQ";
 import Contact from "../components/HomePage/Contact";
 import Footer from "../components/HomePage/Footer";
+import HeroNavbar from "../components/HomePage/HeroNavbar";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div className="relative flex flex-col">
-      {/** <HeroNavbar />*/}
+      <HeroNavbar />
       <NavBar/>
       <Hero />
       <Events />
