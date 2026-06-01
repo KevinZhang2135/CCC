369da9294dc15bb384d61e904f0db1f22c3d9bb3
Kevin Zhang <kevinzhang@Mac.wlan>
2026-05-31 22:11:00 -0400

    gh-pages setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# Campus Coders Crew (beta)
-This website was developed to represent Campus Coders Crew.
--- src/components/HomePage/Footer.tsx
+++
@@ -1,8 +0,0 @@
-// Copyright text
-export default function Footer() {
-  return (
-    <div className="flex justify-center text-base text-ocean-dark">
-      <span>Copyright © 2026 | Campus Coder Crew</span>
-    </div>
-  );
-}
